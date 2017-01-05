6629ae0137fae458f8d0c3c8d57aaad0a7967642
Bertrand Marron <bertrand.marron@ionisx.com>
2017-01-05 01:27:07 +0100

    Add missing whitespaces

--- src/popup/components/Offline/index.jsx
+++ src/popup/components/Offline/index.jsx
@@ -6,7 +6,7 @@ import style from './style.less';
 export default class Offline extends React.Component {
     render() {
         return (
-            <div className={style.block}>
+            <div className={ style.block }>
                 L’ArmaTeam est hors-ligne. :(
             </div>
         );
