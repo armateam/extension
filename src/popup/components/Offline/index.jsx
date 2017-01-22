86e2ebb676f3432f31017b072d1ca5859c8f51ad
Bertrand Marron <bertrand.marron@ionisx.com>
2017-01-22 16:15:54 +0100

    Fix firefox text-align on offline component

--- src/popup/components/Offline/index.jsx
+++ src/popup/components/Offline/index.jsx
@@ -30,7 +30,7 @@ export default class Offline extends React.Component {
                     <img alt='ArmaTeam' src={ logoUrl } />
                 </div>
                 <p className={ style.offline }>{ chrome.i18n.getMessage('popupOfflineMessage') }</p>
-                <div>
+                <div className={ style.links }>
                     <a className={ style.link } onClick={ this.openLink.bind(this, this.twitter) }>
                         <i className="fa fa-twitter" />
                     </a>
